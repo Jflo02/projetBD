28ba9b444194bf2b8c1672207a4cb437ea55c9fa
jflo02 <florimond2002@gmail.com>
2021-03-17 17:01:27 +0100

    fix mineurs + crud client

--- menu.php
+++ menu.php
@@ -1,27 +1,60 @@
 <nav>
-    <ul>
-        <li><a href="./">Acceuil</a></li>
-        <li><a href="./liste_circuits.php?c=test">Circuits</a></li>
-        <?php
-
-        if (isset($_SESSION['type'])) {
-            if ($_SESSION['type'] == "Administrateur") {
-        ?>
-
-                <li class="deroulant"><a href="#">Administration</a>
-                    <ul class="sous">
-                        <li><a href="#">Circuit</a></li>
-                        <li><a href="#">Etape</a></li>
-                    </ul>
-                </li>
+    <table border=1>
+        <tr>
+            <th><a href="./">Acceuil</a></th>
+            <th><a href="./liste_circuits.php?c=default">Circuits</a></th>
 
 
-        <?php
+            <?php
+
+            if (isset($_SESSION['type'])) {
+                if ($_SESSION['type'] == "Administrateur") {
+            ?>
+
+                    <th>
+                        <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+                        <ul class="menulist">
+                            <li>
+                                <p>Administration</p>
+                                <div class="cache">
+                                    
+                                    <a href="./lieu.php">Lieu</a>
+                                    <br>
+                                    <a href="./client.php?c=default">Client</a>
+                                    <br>
+                                    <a href="#">Administrateur</a>
+                                </div>
+                            </li>
+                        </ul>
+                        <script type="text/javascript">
+                            $(function() {
+                                // Quand on entre dans un item de menu <li>, on affiche
+                                $('.menulist li').mouseenter(function() {
+                                    $(this).find('.cache').show('slow');
+                                });
+
+                                // Quand on en sort, on ferme
+                                $('.menulist li').mouseleave(function() {
+                                    $(this).find('.cache').hide('slow');
+                                });
+                            });
+                        </script>
+                    </th>
+
+            <?php
+                }
             }
-        }
 
 
 
-        ?>
-    </ul>
+            ?>
+
+        </tr>
+    </table>
+
+
+
+
+
+
 </nav>
