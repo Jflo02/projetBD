d2a131200147a5288c7d4346dbbde3453d3895c5
jflo02 <florimond2002@gmail.com>
2021-04-02 16:25:10 +0100

    mise en forme tableau + css

--- login.php
+++ login.php
@@ -14,74 +14,79 @@
     include("./en-tete.php");
 
 
+    ?>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+            
+                <?php
+
+
+
+                if (isset($_POST['mdp_user'])) {
+                    //ici on se connecte a la base sql
+                    include("../connection_database.php");
 
 
 
 
-if (isset($_POST['mdp_user'])) {
-    //ici on se connecte a la base sql
-    include("../connection_database.php");
+                    $sql = 'SELECT * from Personne where Personne_Mail=\'' . $_POST["mail_user"] . '\' and Personne_MDP=\'' . $_POST["mdp_user"] . '\'';
+                    $resultat = sqlsrv_query($conn, $sql);
+                    if ($resultat == FALSE) {
+                        die("<br>Echec d'execution de la requete : " . $sql);
+                    } else {
+                        if (sqlsrv_has_rows($resultat) == 1) {
 
-    
+                            $row = sqlsrv_fetch_array($resultat);
+                            $_SESSION['id_user'] = $row['Id_Personne'];
+                            $_SESSION['nom_user'] = $row['Nom'];
+                            $_SESSION['prenom_user'] = $row['Prenom'];
 
-
-    $sql = 'SELECT * from Personne where Personne_Mail=\'' . $_POST["mail_user"] . '\' and Personne_MDP=\'' . $_POST["mdp_user"] . '\'';
-    $resultat = sqlsrv_query($conn, $sql);
-    if ($resultat == FALSE) {
-        die("<br>Echec d'execution de la requete : " . $sql);
-    } else {
-        if (sqlsrv_has_rows($resultat) == 1) {
-
-            $row = sqlsrv_fetch_array($resultat);
-            $_SESSION['id_user'] = $row['Id_Personne'];
-            $_SESSION['nom_user'] = $row['Nom'];
-            $_SESSION['prenom_user'] = $row['Prenom'];
-
-            //on regarde si la personne est administrateur
-            $sql = 'SELECT * FROM Administrateur where Id_Personne =' . $_SESSION['id_user'];
-            $stmt = sqlsrv_query($conn, $sql);
-            $is_lignes = sqlsrv_has_rows($stmt);
-            if ($stmt) { //si la requete est bien effectué
-                if ($is_lignes) { //si id_personne est ds administrateur
-                    $_SESSION['type'] = "Administrateur";
-                } else {
-                    $_SESSION['type'] = "Client";
+                            //on regarde si la personne est administrateur
+                            $sql = 'SELECT * FROM Administrateur where Id_Personne =' . $_SESSION['id_user'];
+                            $stmt = sqlsrv_query($conn, $sql);
+                            $is_lignes = sqlsrv_has_rows($stmt);
+                            if ($stmt) { //si la requete est bien effectué
+                                if ($is_lignes) { //si id_personne est ds administrateur
+                                    $_SESSION['type'] = "Administrateur";
+                                } else {
+                                    $_SESSION['type'] = "Client";
+                                }
+                            }
+                        }
+                    }
                 }
-            }
-
-
-
-        }
-    }
-}
 
 
 
 
 
 
-    if (isset($_SESSION['type'])) {
+                if (isset($_SESSION['type'])) {
 
-        echo 'Hello ' . (($_SESSION['type'] == "Administrateur") ? "Administrateur " : "client ") . $_SESSION['nom_user'] . ' ' . $_SESSION['prenom_user'];
-        echo '<br><a href="./login.php?logout=1">Se deconnecter</a><br><br>';
-        echo '<br><a href="./">Aller à l\'acceuil</a><br><br>';
-    }
+                    echo 'Hello ' . (($_SESSION['type'] == "Administrateur") ? "Administrateur " : "client ") . $_SESSION['nom_user'] . ' ' . $_SESSION['prenom_user'];
+                    echo '<br><a href="./login.php?logout=1">Se deconnecter</a><br><br>';
+                    echo '<br><a href="./">Aller à l\'acceuil</a><br><br>';
+                }
 
 
-    if (!isset($_SESSION['id_user'])) {
-    ?>
-        <form action="./login.php" method="post">
-            <label for="nom">Mail :</label>
-            <input type="text" id="mail_user" name="mail_user"><br><br>
-            <label for="password">Mot de passe :</label>
-            <input type="password" id="mdp_user" name="mdp_user"><br><br>
-            <input type="submit" value="Envoyer">
+                if (!isset($_SESSION['id_user'])) {
+                ?>
+                    <form action="./login.php" method="post">
+                        <label for="nom">Mail :</label>
+                        <input type="text" id="mail_user" name="mail_user"><br><br>
+                        <label for="password">Mot de passe :</label>
+                        <input type="password" id="mdp_user" name="mdp_user"><br><br>
+                        <input type="submit" value="Envoyer">
 
-        </form>
+                    </form>
 
-    <?php
-    }
-    ?>
+                <?php
+                }
+                ?>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
--- moncompte.php
+++ moncompte.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html> 
+<!DOCTYPE html>
 <html>
 
 <head>
@@ -20,70 +20,71 @@
     include("./menu.php");
 
     ?>
-<div id="corps">
+    <div class="container">
 
-<?php
-
-    switch ($_GET['c']){
-        case 'modif':
-            foreach ($_GET as $key => $Value) {
-                if (empty($Value)) {
-                    die("Il manque une valeur pour " . $key);
-                }
-            }
-            $sql = "UPDATE Personne SET Nom='" . $_GET['nom_pers'] . "', Date_Naissance='" . $_GET['DN_pers'] . "', Prenom='" . $_GET['prenom_pers'] . "',Personne_Mail='" . $_GET['mail_pers'] . "',Personne_MDP='" . $_GET['mdp_pers'] . "' where Id_Personne=" . $_GET['id_pers'];
-            $stmt = sqlsrv_query($conn, $sql);
-            if ($stmt == FALSE) {
-                die("<br>Echec d'execution de la requete : " . $sql);
-            } else {
-                echo "Enregistrement mis à jour<br><br>";
-                echo '<a href="./moncompte.php?c=default">Retour à mon compte</a>';
-            }
-        break;
-
-        default:
-        echo "<br><br>";
-        $sql = "SELECT * FROM Personne WHERE Id_Personne=" . $_SESSION['id_user'];
-        $stmt = sqlsrv_query($conn, $sql);
-        if ($stmt == FALSE) {
-            die("<br>Echec d'execution de la requete : " . $sql);
-        } else {
-            
-            while ($row = sqlsrv_fetch_array($stmt)) {
-                $str_date = $row['Date_Naissance']->format('Y-m-d');
-?>
-                <form action="./moncompte.php" method="get">
-                    <label for="nom">Nom :</label>
-                    <input type="text" id="nom_pers" name="nom_pers" value="<?php echo $row['Nom'] ?>"><br><br>
-
-                    <label for="prenom">Prénom :</label>
-                    <input type="text" id="prenom_pers" name="prenom_pers" value="<?php echo $row['Prenom'] ?>"><br><br>
-
-                    <label for="email">Mail :</label>
-                    <input type="email" id="mail_pers" name="mail_pers" value="<?php echo $row['Personne_Mail'] ?>"><br><br>
-
-                    <label for="DN">Date de naissance :</label>
-                    <input type="date" id="DN_pers" name="DN_pers" value="<?php echo $str_date ?>"><br><br>
-                    
-                    <label for="password">Mot de passe :</label>
-                    <input type="password" id="mdp_pers" name="mdp_pers" value="<?php echo $row['Personne_MDP'] ?>" minlength="8" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"><br><br>
-                    <p>8 caractères d'au moins un chiffre et une lettre majuscule et minuscule</p>
-                    <input type="hidden" name="id_pers" value="<?php echo $row['Id_Personne'] ?>">
-                    <input type="hidden" name="c" value="modif">
-
-                    <input type="submit" value="Appuie pour faire les changements">
-                </form>
         <?php
-            }
+
+        switch ($_GET['c']) {
+            case 'modif':
+                foreach ($_GET as $key => $Value) {
+                    if (empty($Value)) {
+                        die("Il manque une valeur pour " . $key);
+                    }
+                }
+                $sql = "UPDATE Personne SET Nom='" . $_GET['nom_pers'] . "', Date_Naissance='" . $_GET['DN_pers'] . "', Prenom='" . $_GET['prenom_pers'] . "',Personne_Mail='" . $_GET['mail_pers'] . "',Personne_MDP='" . $_GET['mdp_pers'] . "' where Id_Personne=" . $_GET['id_pers'];
+                $stmt = sqlsrv_query($conn, $sql);
+                if ($stmt == FALSE) {
+                    die("<br>Echec d'execution de la requete : " . $sql);
+                } else {
+                    echo "Enregistrement mis à jour<br><br>";
+                    echo '<a href="./moncompte.php?c=default">Retour à mon compte</a>';
+                }
+                break;
+
+            default:
+                echo "<br><br>";
+                $sql = "SELECT * FROM Personne WHERE Id_Personne=" . $_SESSION['id_user'];
+                $stmt = sqlsrv_query($conn, $sql);
+                if ($stmt == FALSE) {
+                    die("<br>Echec d'execution de la requete : " . $sql);
+                } else {
+
+                    while ($row = sqlsrv_fetch_array($stmt)) {
+                        $str_date = $row['Date_Naissance']->format('Y-m-d');
+        ?>
+                        <div class="container">
+                            <form action="./moncompte.php" method="get">
+                                <label for="nom">Nom :</label>
+                                <input type="text" id="nom_pers" name="nom_pers" value="<?php echo $row['Nom'] ?>"><br><br>
+
+                                <label for="prenom">Prénom :</label>
+                                <input type="text" id="prenom_pers" name="prenom_pers" value="<?php echo $row['Prenom'] ?>"><br><br>
+
+                                <label for="email">Mail :</label>
+                                <input type="email" id="mail_pers" name="mail_pers" value="<?php echo $row['Personne_Mail'] ?>"><br><br>
+
+                                <label for="DN">Date de naissance :</label>
+                                <input type="date" id="DN_pers" name="DN_pers" value="<?php echo $str_date ?>"><br><br>
+
+                                <label for="password">Mot de passe :</label>
+                                <input type="password" id="mdp_pers" name="mdp_pers" value="<?php echo $row['Personne_MDP'] ?>" minlength="8" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"><br><br>
+                                <p>8 caractères d'au moins un chiffre et une lettre majuscule et minuscule</p>
+                                <input type="hidden" name="id_pers" value="<?php echo $row['Id_Personne'] ?>">
+                                <input type="hidden" name="c" value="modif">
+
+                                <input type="submit" value="Appuie pour faire les changements">
+                            </form>
+                        </div>
+        <?php
+                    }
+                }
         }
-        
-    }   
 
-?>
+        ?>
 
 
-</div>
-<!--On ferme le div du corps -->
+    </div>
+    <!--On ferme le div du corps -->
 </body>
 
 </html>
