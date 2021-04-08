f7d2a70e0d8703c548e2aefc9fa0dfd8f780766c
Nicolas TAHON <nicolas.tahon@epsi.fr>
2021-04-08 14:22:34 +0200

    barre de recherce2

--- recherche.php
+++ recherche.php
@@ -7,7 +7,7 @@
     $DureeMin = $row['DureeMin'];
     $PrixMax = $row['PrixMax'];
     $PrixMin = $row['PrixMin'];
-    $today = date("d-m-Y");
+    $today = date("Y-m-d");
 ?>
 
 <p>
@@ -18,8 +18,13 @@
     <table border=1>
         <tr>
             <form action="./resultat_recherche.php?" method="GET">
+                <?php
+                    if(!isset($_GET['jours']) and !isset($_GET['nombre_passa']) and !isset($_GET['prix'])){
+                ?>
                 <td><label for="date_debut">Date de début</label></td>
-                <td><input type="date" id="date_debut" name="date_debut"></td>
+                <td><input type="date" id="date_debut" name="date_debut" value="<?php echo $today ?>"></td>
+                <td><label for="date_fin">Date de fin</label></td>
+                <td><input type="date" id="date_fin" name="date_fin"></td>
                 <td><label for="jours">Nombre de jours maximum</label></td>
                 <td><input type="range" id="jours" name="jours" min="<?php echo $DureeMin ?>" max="<?php echo $DureeMax ?>" value="<?php echo $DureeMax ?>" oninput="this.nextElementSibling.value = this.value"><output><?php echo $DureeMax ?></output></td>
                 <td><label for="nombre_passa">Nombre de places</label></td>
@@ -27,6 +32,23 @@
                 <td><label for="prix">Prix maximum</label></td>
                 <td><input type="range" id="prix" name="prix" min="<?php echo $PrixMin ?>" max="<?php echo $PrixMax ?>" value="<?php echo $PrixMax ?>" oninput="this.nextElementSibling.value = this.value"><output><?php echo $PrixMax ?></output></td>
                 <td><input type="submit" value="Rechercher"></td>
+                <?php
+                    }else{
+                ?>
+                 <td><label for="date_debut">Date de début</label></td>
+                <td><input type="date" id="date_debut" name="date_debut" value="<?php echo $_GET['date_debut'] ?>"></td>
+                <td><label for="date_fin">Date de fin</label></td>
+                <td><input type="date" id="date_fin" name="date_fin" value="<?php echo $_GET['date_fin'] ?>"></td>
+                <td><label for="jours">Nombre de jours maximum</label></td>
+                <td><input type="range" id="jours" name="jours" min="<?php echo $DureeMin ?>" max="<?php echo $DureeMax ?>" value="<?php echo $_GET['jours'] ?>" oninput="this.nextElementSibling.value = this.value"><output><?php echo $_GET['jours'] ?></output></td>
+                <td><label for="nombre_passa">Nombre de places</label></td>
+                <td><input type="range" id="nombre_passa" name="nombre_passa" min="1" max="20" value="<?php echo $_GET['nombre_passa'] ?>" oninput="this.nextElementSibling.value = this.value"><output><?php echo $_GET['nombre_passa'] ?></output></td>
+                <td><label for="prix">Prix maximum</label></td>
+                <td><input type="range" id="prix" name="prix" min="<?php echo $PrixMin ?>" max="<?php echo $PrixMax ?>" value="<?php echo $_GET['prix'] ?>" oninput="this.nextElementSibling.value = this.value"><output><?php echo $_GET['prix'] ?></output></td>
+                <td><input type="submit" value="Rechercher"></td>
+                <?php
+                    }
+                ?>
             </form>
         </tr>
     </table>
